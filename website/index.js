3c23a26070b5bf8c56a34709285e0cb3d6cfaf69
Jessica Leach <jessicamaryleach@gmail.com>
2025-04-18 15:52:55 +0100

    update counter logic

--- website/index.js
+++ website/index.js
@@ -1,9 +1,8 @@
-const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     let response = await fetch("https://e76tyza5cahp4abj45estcgoam0olzlx.lambda-url.eu-north-1.on.aws/");
     let data = await response.json();
     console.log('data', data)
-    counter.innerHTML = data
+    document.getElementById("counter").innerHTML = data
 }
 
 updateCounter();
